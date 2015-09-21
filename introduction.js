8bbd7cb11feab9b530411b9a4f63f68b5c19d378
chinnaamara <amara.chinna@gmail.com>
2015-09-21 08:07:10 +0530

    add operators

--- introduction.js
+++ introduction.js
@@ -14,10 +14,98 @@
 
 // str =  12;
 
+// expresson and statements
 
+// liteals
+
+var a = 12;
+
+var arr = [] // avoid  var arr = new Array();
+
+// Operators
 var a = 10;  // a is variable  10 is value
 
 var a = 20;
  a = 'abc';
-console.log(a)
+console.log(a);
+
+var a = 10;
+
+var arr = new Array(); // avoid new Array to create arrays
+
+console.log('arr: ', arr);
+
+//alert('');
+
+ // PascalCase -- use it for classes
+ // camelCase -- use it for variables
+
+// function literal
+var add = function(a, b){ // prefer this
+	return a + b;
+};
+
+function sub(a, b){
+	return a - b;
+};
+
+
+
+var eggs = function(){
+	return chicken();
+};
+
+var chicken = function(){
+	return eggs();
+}
+
+//chicken(); // -- Maximum call stack size exceeded
+
+var obj = {}; // object literal
+arr = [] // array literal
+
+// Object
+
+var obj = {
+	a: 12,
+	b: function(val){
+		return val
+	},
+	c: [],
+	d : {}
+};
+
+console.log(obj);
+
+var calculate = {
+	add: function(a, b){
+	 return a + b 
+	},
+
+	sub: function(a , b) {
+		return a - b;
+	}
+};
+
+console.log(calculate.add(10, 20));
+console.log(calculate.sub(50, 30));
+
+// operators
+
+// Arithmatic operatrs + - * / %
+
+// logical operators && || == 
+
+// ! not operator
+
+// =
+
+/* multiline comments
+
+ */
+
+ // inline comment
+
+
+
 
