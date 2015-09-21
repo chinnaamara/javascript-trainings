b742c42445c37e496a3e2899c5945ee81d2ee998
chinnaamara <amara.chinna@gmail.com>
2015-09-21 07:33:53 +0530

    add introductio

---
+++ introduction.js
@@ -0,0 +1,23 @@
+// js is dynamic pl
+// js is imperative
+// js is loosly typing
+// variables are immutable
+// js is OOPL
+// on Objects
+
+// prototypal inheritance in js
+
+// var int = 12;
+// int = 'xfvsd'
+
+// var str = 'dhbf'
+
+// str =  12;
+
+
+var a = 10;  // a is variable  10 is value
+
+var a = 20;
+ a = 'abc';
+console.log(a)
+
