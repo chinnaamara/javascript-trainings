3b92337a466c4bc604d1309650831e2db3d6c439
chinnaamara <amara.chinna@gmail.com>
2015-09-22 08:01:12 +0530

    add Numbers

---
+++ values.js
@@ -0,0 +1,68 @@
+// values -- 0 1 
+
+// 6 datatypes 
+
+// Numbers
+// Strings
+// Booleans
+// Objects
+// Functions
+// Undefined values
+
+//Numbrs:
+
+var a = 23;
+// 2powre 0 to 2 power 63
+
+// 64 types of binary digits used to store a value
+//  +, -, 0 and deciml  
+
+// 2.34
+// 2000 -> 2e3
+
+// 3.45e10 --> 
+
+// Arithmatic operations
+
+console.log(2 + 5 * 10)
+console.log((2 + 5) * 10)
+
+// Precedence of operaors
+
+// * has higher precedence than adition
+
+// bracket has higher precedence than all oprators
+
+// * / % has same precedences
+
+// + and - has equal precedence
+
+// 2 + 5 - 3 // left to right
+
+//(2 + 5) - 3
+
+// Three special Number types
+
+// infinity 
+// -infinity
+// NaN
+
+console.log(10/0);
+console.log(-10/0);
+console.log(10/'a');
+
+//typeof infinity -> number
+
+console.log(typeof infinity);
+
+
+// Strings
+
+// to represent text in quotes
+
+//eg: "new text", 'another text' 
+
+
+var str1 = 'hello';
+var str2 = 'word';
+console.log(str1 + str2);
