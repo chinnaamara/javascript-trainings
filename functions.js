63d6e0fe44ce8fb83a3b1a4d8f7b42ba39a3e5f4
chinnaamara <amara.chinna@gmail.com>
2015-09-23 08:10:36 +0530

    add functions basics

---
+++ functions.js
@@ -0,0 +1,13 @@
+// ==> Functions
+
+// alert() 
+// alert is function name
+// () is used to invoke alert function
+// 
+var add = function(){
+	return arguments[0] + arguments[1];
+}
+
+console.log(add(3, 6));
+
+console.log(2, 4, 6, 8, 10);
